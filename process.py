3629b5479f4f0bc2df9500d8d47f0fe2f1f3ad4f
Марченко Игорь Анатольевич <Marchenko-I@gis.lan>
2023-01-10 09:15:47 +0300

    Python test script

--- process.py
+++ process.py
@@ -3,6 +3,9 @@ from random import randint
 import requests
 import json
 import time
+import brotli
+import base64
+import urllib.parse
 
 def write_to_process(score:str):
     rwm = ReadWriteMemory()
@@ -23,9 +26,35 @@ def get_info():
     data = json.loads(resp.text)
     if data['top'][0]['username'] != "mmimq":
         print(data['top'][0]['username'])
-        write_to_process(int(data['top'][0]['score']))
-        
+        decode_url(int(data['top'][0]['score']))
+
+def decode_url(score:str):
+    headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+        "Host": "telegram.immortal-games.online",
+        "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+
+    }
+    val = score + 100
+    val = "uoTl9ruNQ1jgUCHFK/DTKReHzJb/SElkx9g8iYSuBag="
+    val = urllib.parse.parse_qs("uoTl9ruNQ1jgUCHFK/DTKReHzJb/SElkx9g8iYSuBag".encode('utf-8'))
+    print(val)
+    #print(base64.b64decode(val).decode('utf-8'))
+    session = requests.Session()
+    session.headers = headers
+
+    resp = session.post("https://telegram.immortal-games.online/Games/rest/init.php", data={
+        "query_id": "AAE0sYAcAAAAADSxgBxX5SWz&user=%7B%22id%22%3A478196020%2C%22first_name%22%3A%22%D0%98%D0%B3%D0%BE%D1%80%D1%8C%22%2C%22last_name%22%3A%22%D0%9C%22%2C%22username%22%3A%22mmimq%22%2C%22language_code%22%3A%22en%22%7D&auth_date=1673250689&hash=16dff535decd443c3e23af2a8769e3812461e89d24ac1d4216325080ae888fd8",
+        "hash": "16dff535decd443c3e23af2a8769e3812461e89d24ac1d4216325080ae888fd8",
+        "username": "mmimq",
+        "id":478196020
+    }, headers=headers)
+    print(resp.text)
+    resp = session.post("https://telegram.immortal-games.online/Games/rest/set.php", headers=headers, data=val)
+    print(resp.text, str(val))
+    
+    
 if __name__ == '__main__':
-    while True:
-        get_info()
-        time.sleep(10)
+    get_info()
